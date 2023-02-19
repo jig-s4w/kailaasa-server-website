3e0db8ed9b2a7c04a08f0e9cd66377514467fd41
Tharun Kumar <53345192+jig-s4w@users.noreply.github.com>
2023-02-19 15:00:02 +0530

    Main template done

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const discordServer = "CFyCDZ3V8T"
 
 const discordCount = document.querySelector(".discord-mem-count")
+const tab = document.querySelector(".main-content-image")
+const tabTitle = document.getElementById("main-content-title")
+const tabDesc =document.getElementById("main-content-title-desc")
+
 
 
 fetch( `https://discord.com/api/v9/invites/${discordServer}?with_counts=true&with_expiration=true`)
@@ -14,7 +18,51 @@ setInterval(()=>{
     .then((data) => discordCount.textContent = data.approximate_member_count);
 },20000)
 
-function changestate(){
-    console.log('test')
+function tabSlection(id){
+    switch(id){
+      case "1":
+        console.log("one")
+        // document.getElementById("div1").classList.add("classToBeAdded");
+        tab.style.backgroundImage="url(./images/tab-1-content.jpg)"
+        tabTitle.innerHTML="Title 1"
+        tabDesc.innerHTML="Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+        removeSelected()
+        document.getElementById(id).classList.add("tab-selected")
+        
+        break;
+      case "2":
+        console.log("two")
+        tab.style.backgroundImage="url(./images/tab-2-content.jpg)"
+        tabTitle.innerHTML="Title 2"
+        tabDesc.innerHTML="ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit"
+        removeSelected()
+        document.getElementById(id).classList.add("tab-selected")
+        break;
+      case "3":
+        console.log("three")
+        tab.style.backgroundImage="url(./images/tab-3-content.jpg)"
+        tabTitle.innerHTML="Title 3"
+        tabDesc.innerHTML=" consectetuer adipiscing elit Lorem ipsum dolor sit"
+        removeSelected()
+        document.getElementById(id).classList.add("tab-selected")
+        break;
+      case "4":
+        console.log("four")
+        tab.style.backgroundImage="url(./images/tab-4-content.jpg)"
+        tabTitle.innerHTML="Title 4"
+        tabDesc.innerHTML="dipiscing elit dolor sit amet, consectetuer adipiscing"
+        removeSelected()
+        document.getElementById(id).classList.add("tab-selected")
+        break;
+      default:
+        break;
+    }
 }
-  
+
+
+function removeSelected(){
+  const allElements = document.querySelectorAll('*');
+    allElements.forEach((element) => {
+      element.classList.remove('tab-selected');
+    });
+}
