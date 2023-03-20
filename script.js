dce537c71c61a9348afe33e50f022da3dfb31b67
jig-s4w <tharun.stk04@gmail.com>
2023-03-20 15:05:57 +0530

    new main content added

--- script.js
+++ script.js
@@ -19,44 +19,44 @@ setInterval(()=>{
 },20000)
 
 function tabSlection(id){
-    switch(id){
-      case "1":
-        console.log("one")
-        // document.getElementById("div1").classList.add("classToBeAdded");
-        tab.style.backgroundImage="url(./images/tab-1-content.jpg)"
-        tabTitle.innerHTML="Title 1"
-        tabDesc.innerHTML="Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
-        removeSelected()
-        document.getElementById(id).classList.add("tab-selected")
-        
-        break;
-      case "2":
-        console.log("two")
-        tab.style.backgroundImage="url(./images/tab-2-content.jpg)"
-        tabTitle.innerHTML="Title 2"
-        tabDesc.innerHTML="ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit"
-        removeSelected()
-        document.getElementById(id).classList.add("tab-selected")
-        break;
-      case "3":
-        console.log("three")
-        tab.style.backgroundImage="url(./images/tab-3-content.jpg)"
-        tabTitle.innerHTML="Title 3"
-        tabDesc.innerHTML=" consectetuer adipiscing elit Lorem ipsum dolor sit"
-        removeSelected()
-        document.getElementById(id).classList.add("tab-selected")
-        break;
-      case "4":
-        console.log("four")
-        tab.style.backgroundImage="url(./images/tab-4-content.jpg)"
-        tabTitle.innerHTML="Title 4"
-        tabDesc.innerHTML="dipiscing elit dolor sit amet, consectetuer adipiscing"
-        removeSelected()
-        document.getElementById(id).classList.add("tab-selected")
-        break;
-      default:
-        break;
-    }
+  switch(id){
+    case "1":
+      console.log("one")
+      // document.getElementById("div1").classList.add("classToBeAdded");
+      tab.style.backgroundImage="url(./images/cars.png)"
+      tabTitle.innerHTML="Supercars"
+      tabDesc.innerHTML="Unleash the power of luxury and speed in Kailaasa with our collection of custom supercars. Browse, download, and race to the top!"
+      removeSelected()
+      document.getElementById(id).classList.add("tab-selected")
+      
+      break;
+    case "2":
+      console.log("two")
+      tab.style.backgroundImage="url(./images/gang.png)"
+      tabTitle.innerHTML="Gangs"
+      tabDesc.innerHTML="Take over the city with our gang mods. Rule the streets and dominate the competition in Kailaasa."
+      removeSelected()
+      document.getElementById(id).classList.add("tab-selected")
+      break;
+    case "3":
+      console.log("three")
+      tab.style.backgroundImage="url(./images/peds.png)"
+      tabTitle.innerHTML="Peds"
+      tabDesc.innerHTML="Customize your world with additional peds and characters for a more diverse and dynamic environment."
+      removeSelected()
+      document.getElementById(id).classList.add("tab-selected")
+      break;
+    case "4":
+      console.log("four")
+      tab.style.backgroundImage="url(./images/heist-content.png)"
+      tabTitle.innerHTML="Heists"
+      tabDesc.innerHTML="Experience the thrill of high-stakes robberies in Kailaasa with our collection of custom heist mods."
+      removeSelected()
+      document.getElementById(id).classList.add("tab-selected")
+      break;
+    default:
+      break;
+  }
 }
 
 
